98a73e9a848fd51d5449fd21bad547e4530e9f85
Brian Lawrence <brianlawrence2@gmail.com>
2020-08-08 22:21:52 +0000

    added symlinking for .bashrc to install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,8 @@ sudo apt-get update -y
 echo "Instal packages"
 echo "...nothing here yet"
 
+echo "backing up $HOME/.bashrc if exists and symlinking $(pwd)/.bashrc"
+if [ -f "$HOME/.bashrc" ]; then 
+    mv $HOME/.bashrc $HOME/.bashrc-bkp
+fi
+ln -s $(pwd)/.bashrc $HOME/
