daf2231656b50d5e3177012e3cfaf5951b304564
Brian Lawrence <brianlawrence2@gmail.com>
2020-08-08 22:41:19 +0000

    adding rust to install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,13 @@ sudo apt-get update -y
 echo "Instal packages"
 echo "...nothing here yet"
 
+echo "installing rust"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 echo "backing up $HOME/.bashrc if exists and symlinking $(pwd)/.bashrc"
 if [ -f "$HOME/.bashrc" ]; then 
     mv $HOME/.bashrc $HOME/.bashrc-bkp
 fi
 ln -s $(pwd)/.bashrc $HOME/
+
+
